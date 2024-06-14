6bbf580ab206f0bfee0e885bb32de9335c10cd42
Stuart Daines <s.daines@exeter.ac.uk>
2024-06-14 18:33:14 +0100

    Initial test of new examples/NAPC_tutorial
    NB: data files can be downloaded from
    https://github.com/PALEOtoolkit/PALEOocean.jl/releases/download/v0.4.5/S2P3_transport_20240614.zip
    (unpack zip file in examples/NAPC_tutorial folder)

--- examples/NAPC_tutorial/NAPC_examples.jl
+++ examples/NAPC_tutorial/NAPC_examples.jl
@@ -1,39 +1,43 @@
 using Logging
-# using DiffEqBase
 using OrdinaryDiffEq
 using Sundials
-# using BenchmarkTools
 using Plots
 
 @info "Start $(@__FILE__)"
 
 @info "importing modules ... (may take a few seconds)"
 import PALEOboxes as PB
-# import PALEOreactions
 import PALEOocean
 using PALEOmodel
 @info "                  ... done"
 
 global_logger(ConsoleLogger(stderr,Logging.Info))
 
-include("NAPC_expts.jl")
+# include("NAPC_expts.jl")
 include("NAPC_reactions.jl")
+include("atmreservoirreaction.jl") # for ReactionReservoirAtm
 
-run = NPZ_shelf_expts("P_O2", ["baseline"]); tspan=(0,5.0)
+transport_dir = "S2P3_transport_20240614" # folder containing S2P3 physical variables output collated to netcdf files
 
-initial_state, modeldata = PALEOmodel.initialize!(run)
+# P, O2 only population-based phytoplankton model
+model = PB.create_model_from_config(
+    joinpath(@__DIR__, "NAPC_cfg.yaml"),
+    "NP_shelf";
+    modelpars=Dict(
+        "phys_file"=>joinpath(transport_dir, "S2P3_depth80_m2amp04_phys.nc"),
+        "surf_file"=>joinpath(transport_dir, "S2P3_depth80_m2amp04_surf.nc"),
+    )
+)
 
-# Check initial derivative:
-# initial_deriv = similar(initial_state)
-# PALEOmodel.ODE.ModelODE(modeldata)(initial_deriv, initial_state , (run=run, modeldata=modeldata), 0.0)
-# Check Jacobian:
-# jac, jac_prototype = PALEOmodel.JacobianAD.jac_config_ode(:ForwardDiffSparse, run.model, initial_state, modeldata, 0.0)
-# J = copy(jac_prototype)
-# jac(jac_prototype, initial_state, nothing, 0.0)
+tspan=(0,5.0)
+
+initial_state, modeldata = PALEOmodel.initialize!(model)
+
+paleorun = PALEOmodel.Run(model=model, output = PALEOmodel.OutputWriters.OutputMemory()) 
 
 # first run includes JIT time
 sol = PALEOmodel.ODE.integrateForwardDiff(
-    run, initial_state, modeldata, tspan,
+    paleorun, initial_state, modeldata, tspan,
     solvekwargs=(saveat=1/8760, reltol=1e-5, maxiters=1000000),
 )  
    
@@ -49,23 +53,23 @@ colT=collect(range(start=tspan[1], stop=tspan[end], step=0.5))
 
 # assemble plots onto screens with 6 subplots
 gr(size=(1600, 800)) # gr backend (plotly merges captions with multiple panels)
-pager=PALEOmodel.PlotPager((2, 3), (legend_background_color=nothing, ))
+pager=PALEOmodel.PlotPager((2, 3), (legend_background_color=nothing, margin=(5, :mm)))
 
-pager(plot(title="Total P", run.output, "global.total_P"))
-pager(plot(title="Total O2", run.output, "global.total_O2"))
+pager(plot(title="Total P", paleorun.output, "global.total_P"))
+pager(plot(title="Total O2", paleorun.output, "global.total_O2"))
 pager(:newpage) # flush output
 
-pager(heatmap(title="O2", run.output, "ocean.O2", (column=1, ), clim=(0,Inf)))
-pager(heatmap(title="P", run.output, "ocean.P", (column=1, ), clim=(0,Inf)))
+pager(heatmap(title="O2", paleorun.output, "ocean.O2", (column=1, ), clim=(0,Inf)))
+pager(heatmap(title="P", paleorun.output, "ocean.P", (column=1, ), clim=(0,Inf)))
 
-pager(heatmap(title="P0", run.output, "ocean.P0", (column=1, ), clim=(0,Inf)))
-pager(heatmap(title="P0_growth", run.output, "ocean.P0_growth_rate", (column=1, ), clim=(0,Inf)))
+pager(heatmap(title="P0", paleorun.output, "ocean.P0", (column=1, ), clim=(0,Inf)))
+pager(heatmap(title="P0_growth", paleorun.output, "ocean.P0_growth_rate", (column=1, ), clim=(0,Inf)))
 pager(:newpage) # flush output
 
-# pager(heatmap(title="Z0", run.output, "ocean.Z0", (column=1, ), clim=(0,Inf)))
+# pager(heatmap(title="Z0", paleorun.output, "ocean.Z0", (column=1, ), clim=(0,Inf)))
 # pager(:newpage) # flush output
 
-# pager(heatmap(title="Z0_growth", run.output, "ocean.Z0_growth_rate", (column=1, ), clim=(0,Inf)))
+# pager(heatmap(title="Z0_growth", paleorun.output, "ocean.Z0_growth_rate", (column=1, ), clim=(0,Inf)))
 # pager(:newpage) # flush output
 
 @info "End $(@__FILE__)"
