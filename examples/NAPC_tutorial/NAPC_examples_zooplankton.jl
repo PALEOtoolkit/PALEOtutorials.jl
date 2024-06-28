583bbaf15a383b6d99a1a8624c8aaca1a777bf28
Sara <ss1198@exeter.ac.uk>
2024-06-28 13:05:02 -0600

    Tutorial files for an NPZ example including zooplankton

---
+++ examples/NAPC_tutorial/NAPC_examples_zooplankton.jl
@@ -0,0 +1,75 @@
+# In this file I have changed the file names on lines 19 and 25 and added extra plots for zooplankton line 71
+
+using Logging
+using OrdinaryDiffEq
+using Sundials
+using Plots
+
+@info "Start $(@__FILE__)"
+
+@info "importing modules ... (may take a few seconds)"
+import PALEOboxes as PB
+import PALEOocean
+using PALEOmodel
+@info "                  ... done"
+
+global_logger(ConsoleLogger(stderr,Logging.Info))
+
+# include("NAPC_expts.jl")
+include("NAPC_reactions_zooplankton.jl")
+include("atmreservoirreaction.jl") # for ReactionReservoirAtm
+
+transport_dir = "S2P3_transport_20240614" # folder containing S2P3 physical variables output collated to netcdf files
+
+# P, O2 only population-based phytoplankton model
+model = PB.create_model_from_config(
+    joinpath(@__DIR__, "NAPC_cfg_zooplankton.yaml"),
+    "NP_shelf";
+    modelpars=Dict(
+        "phys_file"=>joinpath(transport_dir, "S2P3_depth80_m2amp04_phys.nc"),
+        "surf_file"=>joinpath(transport_dir, "S2P3_depth80_m2amp04_surf.nc"),
+    )
+)
+
+tspan=(0,5.0)
+
+initial_state, modeldata = PALEOmodel.initialize!(model)
+
+paleorun = PALEOmodel.Run(model=model, output = PALEOmodel.OutputWriters.OutputMemory()) 
+
+# first run includes JIT time
+sol = PALEOmodel.ODE.integrateForwardDiff(
+    paleorun, initial_state, modeldata, tspan,
+    solvekwargs=(saveat=1/8760, reltol=1e-5, maxiters=1000000),
+)  
+   
+########################################
+# Plot output
+########################################
+
+colT=collect(range(start=tspan[1], stop=tspan[end], step=0.5))
+
+# individual plots
+#plotlyjs(size=(500, 300))
+#pager = PALEOmodel.DefaultPlotPager()
+
+# assemble plots onto screens with 6 subplots
+gr(size=(1600, 800)) # gr backend (plotly merges captions with multiple panels)
+pager=PALEOmodel.PlotPager((2, 3), (legend_background_color=nothing, margin=(5, :mm)))
+
+pager(plot(title="Total P", paleorun.output, "global.total_P"))
+pager(plot(title="Total O2", paleorun.output, "global.total_O2"))
+pager(:newpage) # flush output
+
+pager(heatmap(title="O2", paleorun.output, "ocean.O2", (column=1, ), clim=(0,Inf)))
+pager(heatmap(title="P", paleorun.output, "ocean.P", (column=1, ), clim=(0,Inf)))
+
+pager(heatmap(title="P0", paleorun.output, "ocean.P0", (column=1, ), clim=(0,Inf)))
+pager(heatmap(title="P0_growth", paleorun.output, "ocean.P0_growth_rate", (column=1, ), clim=(0,Inf)))
+pager(:newpage) # flush output
+
+pager(heatmap(title="Z0", paleorun.output, "ocean.Z0", (column=1, ), clim=(0,Inf)))
+pager(heatmap(title="Z0_growth", paleorun.output, "ocean.Z0_growth_rate", (column=1, ), clim=(0,Inf)))
+pager(:newpage) # flush output
+
+@info "End $(@__FILE__)"
---
+++ examples/NAPC_tutorial/NAPC_reactions_zooplankton.jl
@@ -0,0 +1,162 @@
+# In this file I have added the zooplankton reaction from line 81
+
+"""
+    Food web biology in 1D shelf setting
+"""
+module ModelShelf
+
+import PALEOboxes as PB
+import Infiltrator # Julia debugger
+
+"""
+    ReactionPhytoplankton
+
+Phytoplankton growth from Armstrong 1994
+
+Photosynthesis reaction: 6 CO2 + 6 H2O -> C6H12O6 + 6 O2
+Stoichiometry photosynthesis O:Corg:N:P 106:106:16:1 
+For each unit P require 106 units Corg and for each unit Corg require 1 units O2
+"""
+Base.@kwdef mutable struct ReactionPhytoplankton{P} <: PB.AbstractReaction
+    base::PB.ReactionBase
+
+    pars::P = PB.ParametersTuple(
+        # Constants for the NPZ model (Armstrong 1994)
+        PB.ParDouble("mumax", 1.4, units="d-1",
+            description="max growth rate"),
+        PB.ParDouble("Ks", 1e-3, units="mol m-3",
+            description="nutrient limitation factor"),
+        PB.ParDouble("insol_scale", 0.005, units="",
+            description="insolation scale factor"),
+        PB.ParDouble("O2_thresh", 1e-6, units="mol m-3",
+            description="oxygen threshold constant"),
+        PB.ParDouble("stoich_PtoO2", -106.0, units="",
+            description="stoichiometry, negative by convention"),
+    )
+
+end
+
+function PB.register_methods!(rj::ReactionPhytoplankton)
+ 
+    # Variables: 
+    # 'Dep' - an external dependency (forcing, reservoir, ...)
+    # 'Prop' - something we calculate
+    # 'Contrib' - a flux we calculate to add to an external Target (eg a reservoir source - sink)
+    vars = [
+        PB.VarDep("insol",          "W m-2",        "insolation"),
+        PB.VarDep("O2_conc",             "mol m-3",          "oxygen conc"),
+        PB.VarContrib("O2_sms",          "mol m-3 yr-1",  "Oxygen source-sink"),
+        PB.VarDep("P_conc",             "mol m-3",          "Nutrient conc"),
+        PB.VarContrib("P_sms",          "mol m-3 yr-1",     "Nutrient source-sink"),
+        PB.VarProp("PX_growth_rate",  "yr-1",             "phytoplankton growth rate"),
+        PB.VarDep("PX_conc",          "mol m-3",          "phytoplankton concentration"),
+        PB.VarContrib("PX_sms",       "mol m-3 yr-1",     "phytoplankton source-sink"),
+    ]
+    PB.add_method_do!(rj, grow_phyto, (PB.VarList_namedtuple(vars),))
+
+    return nothing
+end
+
+function grow_phyto(m::PB.ReactionMethod, (vars, ), cellrange::PB.AbstractCellRange, deltat)
+    rj = m.reaction
+    
+    mumax_yr = rj.pars.mumax.v * PB.Constants.k_daypyr # convert d-1 to yr-1
+    @inbounds for i in cellrange.indices
+        # NB: for numerical stability, defend against -ve values by setting rate to 0
+        # yr-1
+
+        vars.PX_growth_rate[i] = rj.pars.insol_scale.v*vars.insol[i]*mumax_yr*max(vars.P_conc[i], 0.0)/(max(vars.P_conc[i], 0.0) + rj.pars.Ks.v)
+
+        nutrient_rate = max(vars.PX_conc[i], 0.0)*vars.PX_growth_rate[i]
+        
+        vars.PX_sms[i] += nutrient_rate
+        vars.P_sms[i] -= nutrient_rate
+        vars.O2_sms[i] -= rj.pars.stoich_PtoO2.v*nutrient_rate
+       
+    end
+
+    return nothing
+end
+
+"""
+    ReactionZooplankton
+
+Zooplankton growth from Armstrong 1994
+
+Zooplankton growth stops below threshold phytoplankton concentration
+
+Stoichiometry respiration O:Corg:N:P 106:106:16:1
+"""
+Base.@kwdef mutable struct ReactionZooplankton{P} <: PB.AbstractReaction
+    base::PB.ReactionBase
+
+    pars::P = PB.ParametersTuple(
+        # Constants for the NPZ model (Armstrong 1994)
+        PB.ParDouble("gamma", 0.4, units="",
+            description="assimilation efficiency"),
+        PB.ParDouble("Hmax", 1.4, units="d-1",
+            description="max harvest rate"),
+        PB.ParDouble("Kp", 2e-3, units="mol m-3",
+            description="full-saturation constant"), 
+        PB.ParDouble("phyt_thresh", 1e-6, units="mol m-3",
+            description="feeding threshold constant"),
+        PB.ParDouble("O2_thresh", 1e-6, units="mol m-3",
+            description="oxygen threshold constant"),
+        PB.ParDouble("stoich_PtoO2", -106.0, units="",
+            description="stoichiometry"),
+    )
+
+end
+
+function PB.register_methods!(rj::ReactionZooplankton)
+ 
+    # Variables: 
+    # 'Dep' - an external dependency (forcing, reservoir, ...)
+    # 'Prop' - something we calculate
+    # 'Contrib' - a flux we calculate to add to an external Target (eg a reservoir source - sink)
+    vars = [
+        PB.VarProp("ZX_growth_rate",  "yr-1",             "zooplankton growth rate"),
+        PB.VarDep("PX_conc",          "mol m-3",          "phytoplankton concentration"),
+        PB.VarDep("ZX_conc",          "mol m-3",          "zooplankton concentration"),
+        PB.VarContrib("ZX_sms",       "mol m-3 yr-1",     "zooplankton source-sink"),
+        PB.VarContrib("PX_sms",       "mol m-3 yr-1",     "phytoplankton source-sink"),
+        PB.VarContrib("P_sms",          "mol m-3 yr-1",     "Nutrient source-sink"),
+        PB.VarDep("O2_conc",             "mol m-3",          "oxygen conc"),
+        PB.VarContrib("O2_sms",          "mol m-3 yr-1",     "oxygen source-sink"),
+    ]
+    PB.add_method_do!(rj, grow_zoo, (PB.VarList_namedtuple(vars),))
+
+    return nothing
+end
+
+function grow_zoo(m::PB.ReactionMethod, (vars, ), cellrange::PB.AbstractCellRange, deltat)
+    rj = m.reaction
+
+    Hmax_yr = rj.pars.Hmax.v * PB.Constants.k_daypyr # convert d-1 to yr-1
+    HoverK_yr = Hmax_yr/rj.pars.Kp.v
+    @inbounds for i in cellrange.indices
+        # NB: for numerical stability, defend against -ve values by setting rate to 0
+        # yr-1
+        if (vars.PX_conc[i] > rj.pars.phyt_thresh.v) && (vars.O2_conc[i] > rj.pars.O2_thresh.v)
+            vars.ZX_growth_rate[i] = max(vars.PX_conc[i] - rj.pars.phyt_thresh.v, 0.0)*(rj.pars.gamma.v*HoverK_yr)
+        else
+            vars.ZX_growth_rate[i] = 0.0         
+        end
+        
+        phytoplankton_consumption_rate = max(vars.ZX_conc[i], 0.0)*vars.ZX_growth_rate[i]
+
+        # here we are accounting for the fact that zooplankton have an imperfect assimilation efficiency of the plankton they consume 
+        # the plankton nutrient they don't incorporate into their biomass (set by gamma) has to go back into the nutrient pool of the ocean
+        vars.ZX_sms[i] += phytoplankton_consumption_rate 
+        vars.PX_sms[i] -= phytoplankton_consumption_rate / rj.pars.gamma.v
+        
+        phytoplankton_nutrient_release = (phytoplankton_consumption_rate / rj.pars.gamma.v) - phytoplankton_consumption_rate
+        vars.P_sms[i] += phytoplankton_nutrient_release
+        vars.O2_sms[i] += phytoplankton_nutrient_release*rj.pars.stoich_PtoO2.v
+       
+    end
+
+    return nothing
+end
+
+end #end module
